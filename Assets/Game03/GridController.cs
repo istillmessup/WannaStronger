886b1020ddbcb404c6480f99d05a6d12bdf62e7e
istillmessup <1650835292@qq.com>
2020-10-12 23:32:29 +0800

    10.12

---
+++ Assets/Game03/GridController.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Game03
+{
+    public class GridController : MonoBehaviour
+    {
+        public int x, y;
+        public Color color;
+
+        private Vector3 positionStart;
+        private Vector3 positionEnd;
+        private bool isMouseDown = false;
+        private void Update()
+        {
+            if (isMouseDown && Input.GetMouseButtonUp(0) && MapManager.flag)
+            {
+                positionEnd = Input.mousePosition;
+                Vector3 delta = positionEnd - positionStart;
+                isMouseDown = false;
+                // 这些移动控制代码可以尝试用函数包装一下
+                if (delta.x < 0 && Mathf.Abs(delta.x) > Mathf.Abs(delta.y))
+                {
+                    GetComponentInParent<MapManager>().GridLeftMove(y);
+                    Check();
+                }
+                if (delta.x > 0 && Mathf.Abs(delta.x) > Mathf.Abs(delta.y))
+                {
+                    GetComponentInParent<MapManager>().GridRightMove(y);
+                    Check();
+                }
+                if (delta.y < 0 && Mathf.Abs(delta.x) < Mathf.Abs(delta.y))
+                {
+                    GetComponentInParent<MapManager>().GridDownMove(x);
+                    Check();
+                }
+                if (delta.y > 0 && Mathf.Abs(delta.x) < Mathf.Abs(delta.y))
+                {
+                    GetComponentInParent<MapManager>().GridUpMove(x);
+                    Check();
+                }
+            }
+        }
+
+        public void OnPointerDown()
+        {
+            if (MapManager.flag)
+            {
+                isMouseDown = true;
+                positionStart = Input.mousePosition;
+            }
+        }
+
+        private void Check()
+        {
+            if (MapManager._instance.Check())
+            {
+                Debug.Log("game over");
+            }
+        }
+    }
+}
+
--- Assets/Game03/MapManager.cs
+++ Assets/Game03/MapManager.cs
@@ -2,22 +2,74 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using DG.Tweening;
 
 namespace Game03
 {
     public class MapManager : MonoBehaviour
     {
+        public static MapManager _instance;
+        public static bool flag = false;
+
         public GameObject gridPrefab;
         public Color[] color;
+        public Button button1, button2, button3;
 
+        private Dictionary<Vector2, Transform> gridDict = new Dictionary<Vector2, Transform>();
         private List<Color> gridColorList = new List<Color>();
         private Transform gridParent;
 
         private void Awake()
         {
+            _instance = this;
+            flag = false;
             gridParent = GameObject.Find("GridParent").transform;
+            button1.onClick.AddListener(() =>
+            {
+                flag = true;
+                button1.gameObject.SetActive(false);
+                button2.gameObject.SetActive(true);
+                for (int i = 0; i < 5; i++)
+                {
+                    for (int j = 0; j < 5; j++)
+                    {
+                        gridDict[new Vector2(i, j)].GetComponent<Image>().color = Color.gray;
+                    }
+                }
+            });
+            button2.onClick.AddListener(() =>
+            {
+                flag = false;
+                button2.gameObject.SetActive(false);
+                button3.gameObject.SetActive(true);
+                for (int i = 0; i < 5; i++)
+                {
+                    for (int j = 0; j < 5; j++)
+                    {
+                        gridDict[new Vector2(i, j)].GetComponent<Image>().color = gridDict[new Vector2(i, j)].GetComponent<GridController>().color;
+                    }
+                }
+            });
+            button3.onClick.AddListener(() =>
+            {
+                flag = true;
+                button3.gameObject.SetActive(false);
+                button2.gameObject.SetActive(true);
+                for (int i = 0; i < 5; i++)
+                {
+                    for (int j = 0; j < 5; j++)
+                    {
+                        gridDict[new Vector2(i, j)].GetComponent<Image>().color = Color.gray;
+                    }
+                }
+            });
             SetColorList();
             Init();
+        }
+
+        private void Update()
+        {
+            
         }
 
         private void Init()
@@ -28,8 +80,13 @@ namespace Game03
                 {
                     GameObject grid = Instantiate(gridPrefab, gridParent);
                     grid.transform.localPosition = new Vector2(i * 100, j * 100);
+                    grid.GetComponent<GridController>().x = i;
+                    grid.GetComponent<GridController>().y = j;
+                    grid.GetComponent<GridController>().color = gridColorList[0];
                     grid.GetComponent<Image>().color = gridColorList[0];
                     gridColorList.RemoveAt(0);
+
+                    gridDict.Add(new Vector2(i, j), grid.transform);
                 }
             }
         }
@@ -50,6 +107,162 @@ namespace Game03
                 gridColorList.Add(list[r]);
                 list.RemoveAt(r);
             }
+        }
+
+        public void GridLeftMove(int y)
+        {
+            for (int i = 4; i >= 0; i--)
+            {
+                if (i != 0)
+                {
+                    gridDict[new Vector2(i, y)].GetComponent<GridController>().x--;
+                    gridDict[new Vector2(i, y)].DOLocalMoveX((i - 1) * 100, .3f);
+                }
+                else
+                {
+                    gridDict[new Vector2(i, y)].GetComponent<GridController>().x = 4;
+                    GameObject go = gridDict[new Vector2(i, y)].gameObject;
+                    go.SetActive(false);
+                    gridDict[new Vector2(i, y)].DOLocalMoveX(400, .2f).OnComplete(() =>
+                    {
+                        go.SetActive(true);
+                    });
+                }
+            }
+            Transform temp = gridDict[new Vector2(0, y)];
+            for (int i = 0; i < 5; i++)
+            {
+                if (i != 4)
+                {
+                    gridDict[new Vector2(i, y)] = gridDict[new Vector2(i + 1, y)];
+                }
+                else
+                {
+                    gridDict[new Vector2(i, y)] = temp;
+                }
+            }
+        }
+
+        public void GridRightMove(int y)
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                if (i != 4)
+                {
+                    gridDict[new Vector2(i, y)].GetComponent<GridController>().x++;
+                    gridDict[new Vector2(i, y)].DOLocalMoveX((i + 1) * 100, .3f);
+                }
+                else
+                {
+                    gridDict[new Vector2(i, y)].GetComponent<GridController>().x = 0;
+                    GameObject go = gridDict[new Vector2(i, y)].gameObject;
+                    go.SetActive(false);
+                    gridDict[new Vector2(i, y)].DOLocalMoveX(0, .2f).OnComplete(() =>
+                    {
+                        go.SetActive(true);
+                    });
+                }
+            }
+            Transform temp = gridDict[new Vector2(4, y)];
+            for (int i = 4; i >= 0; i--)
+            {
+                if (i != 0)
+                {
+                    gridDict[new Vector2(i, y)] = gridDict[new Vector2(i - 1, y)];
+                }
+                else
+                {
+                    gridDict[new Vector2(i, y)] = temp;
+                }
+            }
+        }
+
+        public void GridDownMove(int x)
+        {
+            for (int i = 4; i >= 0; i--)
+            {
+                if (i != 0)
+                {
+                    gridDict[new Vector2(x, i)].GetComponent<GridController>().y--;
+                    gridDict[new Vector2(x, i)].DOLocalMoveY((i - 1) * 100, .3f);
+                }
+                else
+                {
+                    gridDict[new Vector2(x, i)].GetComponent<GridController>().y = 4;
+                    GameObject go = gridDict[new Vector2(x, i)].gameObject;
+                    go.SetActive(false);
+                    gridDict[new Vector2(x, i)].DOLocalMoveY(400, .2f).OnComplete(() =>
+                    {
+                        go.SetActive(true);
+                    });
+                }
+            }
+            Transform temp = gridDict[new Vector2(x, 0)];
+            for (int i = 0; i < 5; i++)
+            {
+                if (i != 4)
+                {
+                    gridDict[new Vector2(x, i)] = gridDict[new Vector2(x, i + 1)];
+                }
+                else
+                {
+                    gridDict[new Vector2(x, i)] = temp;
+                }
+            }
+        }
+
+        public void GridUpMove(int x)
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                if (i != 4)
+                {
+                    gridDict[new Vector2(x, i)].GetComponent<GridController>().y++;
+                    gridDict[new Vector2(x, i)].DOLocalMoveY((i + 1) * 100, .3f);
+                }
+                else
+                {
+                    gridDict[new Vector2(x, i)].GetComponent<GridController>().y = 0;
+                    GameObject go = gridDict[new Vector2(x, i)].gameObject;
+                    go.SetActive(false);
+                    gridDict[new Vector2(x, i)].DOLocalMoveY(0, .2f).OnComplete(() =>
+                    {
+                        go.SetActive(true);
+                    });
+                }
+            }
+            Transform temp = gridDict[new Vector2(x, 4)];
+            for (int i = 4; i >= 0; i--)
+            {
+                if (i != 0)
+                {
+                    gridDict[new Vector2(x, i)] = gridDict[new Vector2(x, i - 1)];
+                }
+                else
+                {
+                    gridDict[new Vector2(x, i)] = temp;
+                }
+            }
+        }
+
+        public bool Check()
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                Color[] temp = new Color[5];
+                for (int j = 0; j < 5; j++)
+                {
+                    temp[j] = gridDict[new Vector2(j, i)].GetComponent<GridController>().color;
+                }
+                for (int j = 1; j < 5; j++)
+                {
+                    if (temp[0] != temp[j])
+                    {
+                        return false;
+                    }
+                }
+            }
+            return true;
         }
     }
 }
