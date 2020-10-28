f5054adc1855293b1ecdb373d682df4a350b79c8
istillmessup <1650835292@qq.com>
2020-10-28 21:14:37 +0800

    10.28

---
+++ Assets/Game06/GridController.cs
@@ -0,0 +1,135 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+
+namespace Game06
+{
+    public class GridController : MonoBehaviour
+    {
+        public int direction; // 指针的方向
+        
+        public int x, y;
+        public ColorType colorType;
+
+        private void Awake()
+        {
+            GetComponent<Button>().onClick.AddListener(() =>
+            {
+                for (int i = 0; i < 7; i++)
+                {
+                    MapManager._instance.gridDict[new Vector2(0, i)].GetComponent<Button>().enabled = false;
+                }
+                Rotate();
+            });
+            
+        }
+
+        private void Rotate()
+        {
+            
+            if (colorType == ColorType.Green)
+            {
+                transform.DOLocalRotate(new Vector3(0, 0, transform.localEulerAngles.z - 90), .3f).OnComplete(() =>
+                {
+                    direction++;
+                    if (direction > 3)
+                    {
+                        direction = 0;
+                    }
+                    if (direction == 0) // 向上
+                    {
+                        if (y == 6)
+                        {
+                            return;
+                        }
+                        else
+                        {
+                            MapManager._instance.gridDict[new Vector2(x, y + 1)].GetComponent<GridController>().Rotate();
+                        }
+                    }
+                    else if (direction == 1) // 向右
+                    {
+                        if (x == 6)
+                        {
+                            print("你赢了，真牛");
+                            return;
+                        }
+                        else
+                        {
+                            MapManager._instance.gridDict[new Vector2(x + 1, y)].GetComponent<GridController>().Rotate();
+                        }
+                    }
+                    else if (direction == 2) // 向下
+                    {
+                        if (y == 0)
+                        {
+                            return;
+                        }
+                        else
+                        {
+                            MapManager._instance.gridDict[new Vector2(x, y - 1)].GetComponent<GridController>().Rotate();
+                        }
+                    }
+                    else if (direction == 3) // 向左
+                    {
+                        if (x == 0)
+                        {
+                            return;
+                        }
+                        else
+                        {
+                            MapManager._instance.gridDict[new Vector2(x - 1, y)].GetComponent<GridController>().Rotate();
+                        }
+                    }
+                });
+            }
+            else if(colorType == ColorType.Purple)
+            {
+                transform.DOLocalRotate(new Vector3(0, 0, transform.localEulerAngles.z + 90), .3f).OnComplete(() =>
+                {
+                    direction++;
+                    if (direction > 3)
+                    {
+                        direction = 0;
+                    }
+                    if (direction == 0) // 向上
+                    {
+                        MapManager._instance.gridDict[new Vector2(x, y + 1)].GetComponent<GridController>().Rotate();
+                    }
+                    else if (direction == 1) // 向左
+                    {
+                        if (x == 0)
+                        {
+                            return;
+                        }
+                        else
+                        {
+                            MapManager._instance.gridDict[new Vector2(x - 1, y)].GetComponent<GridController>().Rotate();
+                        }
+                    }
+                    else if (direction == 2) // 向下
+                    {
+                        MapManager._instance.gridDict[new Vector2(x, y - 1)].GetComponent<GridController>().Rotate();
+                    }
+                    else if (direction == 3) // 向右
+                    {
+                        if (x == 6)
+                        {
+                            print("你赢了，真牛");
+                            return;
+                        }
+                        else
+                        {
+                            MapManager._instance.gridDict[new Vector2(x + 1, y)].GetComponent<GridController>().Rotate();
+                        }
+                    }
+                });
+            }
+            
+        }
+
+    }
+}
+
---
+++ Assets/Game06/MapManager.cs
@@ -0,0 +1,116 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Game06
+{
+    public class MapManager : MonoBehaviour
+    {
+        public static MapManager _instance;
+
+        public Button button;
+        public GameObject gridPrefab_green;
+        public GameObject gridPrefab_purple;
+        public Dictionary<Vector2, GameObject> gridDict = new Dictionary<Vector2, GameObject>();
+
+        private void Awake()
+        {
+            _instance = this;
+            Init();
+            button.onClick.AddListener(() =>
+            {
+                for (int i = 0; i < 7; i++)
+                {
+                    gridDict[new Vector2(0, i)].GetComponent<Button>().enabled = true;
+                }
+            });
+        }
+
+        private void Init()
+        {
+            for (int i = 0; i < 7; i++)  // y
+            {
+                for (int j = 0; j < 7; j++) // x
+                {
+                    GameObject grid;
+                    if (i % 2 == 0)
+                    {
+                        grid = Instantiate(gridPrefab_green, transform.Find("GridParent"));
+                    }
+                    else
+                    {
+                        grid = Instantiate(gridPrefab_purple, transform.Find("GridParent"));
+                    }
+                    if (j != 0)
+                    {
+                        grid.GetComponent<Button>().enabled = false;
+                    }
+                    grid.transform.localPosition = new Vector2(j * 100, i * 100);
+                    gridDict.Add(new Vector2(j, i), grid);
+                    grid.GetComponent<GridController>().x = j;
+                    grid.GetComponent<GridController>().y = i;
+                    int r = Random.Range(0, 4);
+                    SetGridDirection((DirectionType)r, grid);
+                }
+            }
+        }
+
+        private void SetGridDirection(DirectionType dt, GameObject go)
+        {
+            switch (dt)
+            {
+                case DirectionType.Up:
+                    go.GetComponent<GridController>().direction = 0;
+                    go.transform.localEulerAngles = new Vector3(0, 0, 0);
+                    break;
+                case DirectionType.Left:
+                    if (go.GetComponent<GridController>().colorType == ColorType.Green)
+                    {
+                        go.GetComponent<GridController>().direction = 3;
+                        go.transform.localEulerAngles = new Vector3(0, 0, 90);
+                    }
+                    else
+                    {
+                        go.GetComponent<GridController>().direction = 1;
+                        go.transform.localEulerAngles = new Vector3(0, 0, 90);
+                    }
+                    break;
+                case DirectionType.Down:
+                    go.GetComponent<GridController>().direction = 2;
+                    go.transform.localEulerAngles = new Vector3(0, 0, 180);
+                    break;
+                case DirectionType.Right:
+                    if (go.GetComponent<GridController>().colorType == ColorType.Green)
+                    {
+                        go.GetComponent<GridController>().direction = 1;
+                        go.transform.localEulerAngles = new Vector3(0, 0, -90);
+                    }
+                    else
+                    {
+                        go.GetComponent<GridController>().direction = 3;
+                        go.transform.localEulerAngles = new Vector3(0, 0, -90);
+                    }
+                    break;
+                default:
+                    break;
+
+            }
+        }
+    }
+
+    public enum ColorType
+    {
+        Green,
+        Purple
+    }
+
+    public enum DirectionType
+    {
+        Up,
+        Left,
+        Down,
+        Right
+    }
+}
+
